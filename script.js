70e85a0b2feb097be19d791e0a4e4f6e6592b4c6
wnduldy <2511011@br.hs.kr>
2026-03-24 00:07:56 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const assignments = [
-{subject:"국어", date:"03-26 3교시", content:"현대소설 감상 및 분석 - 독서일지(2차시/10차시"}
+{subject:"국어", date:"03-26 목요일 3교시", content:"현대소설 감상 및 분석 - 독서일지(2차시/10차시",
+  subject:"영어", date:"03-27 금요일 4교시", content:"<지속가능발전목표 달성 실천 제안서 작성> 쓰기 수행평가 개요 작성"}
 ];
 
 // 날짜순 정렬
@@ -17,7 +18,7 @@ list.appendChild(li);
 }
 
 // 급식 (임시 데이터 → 나중에 API 연결 가능)
-const mealText = "차수수밥, 짬뽕국, 가지양파무침, 숫불맛닭갈비볶음, 누룽지꿔바로우/소스, 배추김치, 패션후르츠주스";
+const mealText = "케이준치킨샐러드, 투움바파스타(베이컨), 고르곤졸라피자, 미니밥(자율), 피크닉청포도맛, 오이피클, 배추김치";
 
 if(document.getElementById("meal")){
 document.getElementById("meal").innerText = mealText;
